3bb19b01e518be9334bdec5dcf469311b44ca2eb
Archimède Body <Archimedebody@rocketmail.com>
2021-01-12 11:51:49 +0100

    Re order the files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const local = require ('./local')
-const international = require ('./international')
-const operator = require ('./operator')
-const isValid = require ('./isValid')
+const local = require ('./src/local')
+const international = require ('./src/international')
+const operator = require ('./src/operator')
+const isValid = require ('./src/isValid')
 
 module.exports = {local, international, operator, isValid}
