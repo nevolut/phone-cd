81801969b682e9c545532733aa43e61861f34416
Archimede Body <archimedebody@rocketmail.com>
2023-04-29 04:20:00 +0100

    Update

--- src/national.js
+++ src/national.js
@@ -6,7 +6,7 @@
  */
 exports.__esModule = true;
 var national = function (value) {
-    var number = +value;
+    var number = +String(value).replace(/\D/g, "");
     if (number.toString().substr(0, 4) == "+243")
         number = +number.toString().slice(4);
     else if (number.toString().substr(0, 3) == "243")
